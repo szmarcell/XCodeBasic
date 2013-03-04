b353da647a4cf06a031231627d2694f161563622
szmarcell <szmarcell@index.hu>
2013-03-04 10:05:36 +0100

    Ez remélem sikeres lesz

---
+++ gitHubTestApp/gitHubTestApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  gitHubTestApp
+//
+//  Created by Szabó Marcell on 3/4/13.
+//  Copyright (c) 2013 Szabó Marcell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
